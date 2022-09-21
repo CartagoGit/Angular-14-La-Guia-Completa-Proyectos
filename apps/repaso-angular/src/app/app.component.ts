c925d103c2e6c14c84f2164cc7585e5fa61b1b36
Cartago <cartago.relaxingcup@gmail.com>
2022-09-21 22:14:39 +0200

    Terminamos la seccion de databinding

--- apps/repaso-angular/src/app/app.component.ts
+++ apps/repaso-angular/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cartago-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
-  title = 'repaso-angular';
+  title = '01 - Repaso de Angular';
+  nombre = 'Juan';
+  textoPlaceholder = 'Escriba Algo Aquí biribiribiririririri';
+  public deshabilitado = false;
+  imgSrc =
+    'https://cas-training.com/wp-content/uploads/2020/09/angular-todo-lo-que-necesitas-saber.png';
+  texto = ' Esto es una clase sobre event binding';
+  texto2wdb = '';
+
+  constructor() {
+    setInterval(() => {
+      this.nombre = 'Tomas';
+    }, 2000);
+    setInterval(() => {
+      this.deshabilitado = true;
+      this.textoPlaceholder =
+        'JAJAJJA tardaste demasiado en escribir, te quedaste con las ganas de poder escribir en este input deshabilitado WUAJAJAJ';
+    }, 2000);
+  }
+
+  getSuma(numero1: number, numbero2: number) {
+    return numero1 + numbero2;
+  }
+
+  botonClickeado() {
+    this.texto = 'Hemos cambiado el texto con un event binding!';
+  }
 }
