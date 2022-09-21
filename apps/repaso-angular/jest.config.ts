acf3fa460579b04f867935d6afd9b93e6f6f9a74
Cartago <cartago.relaxingcup@gmail.com>
2022-09-21 20:36:05 +0200

    Dejamos listo nx sin numeros en las apps para que todo funcioen correctamente

--- apps/repaso-angular/jest.config.ts
+++ apps/repaso-angular/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
-  displayName: '2-piano-app-piano-app',
-  preset: '../../../jest.preset.js',
+  displayName: 'repaso-angular',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/apps/2-piano-app/piano-app',
+  coverageDirectory: '../../coverage/apps/repaso-angular',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
--- apps/repaso-angular/src/app/app.component.ts
+++ apps/repaso-angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class AppComponent {
-  title = '1-repaso-angular-repaso-angular';
+  title = 'repaso-angular';
 }
