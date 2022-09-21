4b627b2e260e086823ced18d408679ce2892c004
Cartago <cartago.relaxingcup@gmail.com>
2022-09-21 22:40:46 +0200

    reorganizamos el proyecto de repaso y creamos el componente directivas

--- apps/repaso-angular/src/app/app.module.ts
+++ apps/repaso-angular/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { DirectivasComponent } from './directivas/directivas.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DatabindingComponent, DirectivasComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/repaso-angular/src/app/directivas/directivas.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'directivas',
+  templateUrl: './directivas.component.html',
+  styles: [],
+})
+export class DirectivasComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
