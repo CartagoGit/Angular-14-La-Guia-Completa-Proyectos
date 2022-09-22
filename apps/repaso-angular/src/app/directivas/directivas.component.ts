4ac22cb2a31fb9c75882ab056dbdd933ec7e8a71
Cartago <cartago.relaxingcup@gmail.com>
2022-09-22 02:14:18 +0200

    Terminamos el repaso de angulary continuamos con la primera app

--- apps/repaso-angular/src/app/directivas/directivas.component.ts
+++ apps/repaso-angular/src/app/directivas/directivas.component.ts
@@ -1,12 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+interface IEstudiante {
+  nombre: string;
+  estado: string;
+}
 
 @Component({
-  selector: 'directivas',
-  templateUrl: './directivas.component.html',
+  selector: "directivas",
+  templateUrl: "./directivas.component.html",
   styles: [],
 })
 export class DirectivasComponent implements OnInit {
+  listEstudiantes: IEstudiante[] = [
+    {
+      nombre: "Tomas Gonzalez",
+      estado: "Promocionado",
+    },
+    {
+      nombre: "Marcos Sanchez",
+      estado: "Suspenso",
+    },
+    {
+      nombre: "Marta Lopez",
+      estado: "Aprobado",
+    },
+    {
+      nombre: "Pepe Palotes",
+      estado: "Promocionado",
+    },
+    {
+      nombre: "Maria Regla",
+      estado: "Promocionado",
+    },
+    {
+      nombre: "Oscar Ternario",
+      estado: "Aprobado",
+    },
+    {
+      nombre: "Jose Luis Pedroso",
+      estado: "Suspenso",
+    },
+  ];
+
+  mostrar = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  mostrarToggle = () => {
+    this.mostrar = !this.mostrar;
+  };
 }
